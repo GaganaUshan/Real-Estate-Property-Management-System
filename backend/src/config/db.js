20c1599b8c098ab1f9c30b9c428789d096cbbb32
Gagana Yushan <it22254252@my.sliit.lk>
2025-08-10 22:42:25 +0530

    Mongo DB setting up

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,11 @@
-// backend/src/config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
